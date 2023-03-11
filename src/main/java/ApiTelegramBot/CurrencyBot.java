659e6017f7c3930b6018d71c8edf541a6aba6538
S0lolAlex <lesha1984231@gmail.com>
2023-03-11 18:50:40 +0200

    try koding

--- src/main/java/ApiTelegramBot/CurrencyBot.java
+++ src/main/java/ApiTelegramBot/CurrencyBot.java
@@ -15,7 +15,7 @@ public class CurrencyBot extends TelegramLongPollingBot implements BotCommands {
 
     @Override
     public String getBotUsername() {
-        return "nickname";
+        return "bot";
     }
 
     @Override
